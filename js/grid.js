cbe7a2117a0acd532e31496b1320f1de44abb4d9
Korbow <korbow.contact@gmail.com>
2025-08-20 14:53:16 +0200

    fixs and win animation

--- js/grid.js
+++ js/grid.js
@@ -36,6 +36,45 @@ export function createGrid([rows, cols, cmines]) {
       cell.addEventListener("click", () => {
         reveal(cell, i, nRows, nCols, allCells);
       });
+
+
+
+      cell.addEventListener("contextmenu", (event) => {
+        event.preventDefault(); // empêche le menu clic droit
+      
+        if (cell.dataset.flaged === "true") {
+          console.log("Déjà flaguée !");
+          return; // on arrête, la case est déjà flaguée
+        }
+        if (cell.classList.contains("revealed")) {
+          console.log("Déjà révélé !");
+          return; // on arrête, la case est déjà révélée
+        }
+
+        cell.dataset.flaged = "true";
+        setTimeout(() => {
+        cell.innerHTML = `
+          <div class="flag">
+            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
+              <rect x="10" y="5" width="3" height="30" fill="#333"/>
+              <path class="flag-cloth" d="M13 5 L30 10 L13 15 Z" fill="#e63946"/>
+            </svg>
+          </div>`;
+
+
+        const flag = cell.querySelector(".flag");
+        console.log(flag);
+        setTimeout(() => {
+        flag.classList.add("anim");
+        }, 100);
+      }, 10);
+
+
+
+
+      });
+        
+      
   
       gameGrid.appendChild(cell);
     }
--- js/reveal.js
+++ js/reveal.js
@@ -3,14 +3,75 @@ import { setGameEnded } from './TimerEtScore.js';
 import { getTotalMines } from './mines.js';
 
 
+
 function gameOver() {
   setGameEnded(true);
   document.dispatchEvent(new Event("gameOver"));
 }
 function gameWin() {
   setGameEnded(true);
+
+
+  /* Effet confetti */
+
+  const overlay = document.getElementById("confetti-overlay");
+  const emoji = document.createElement("div");
+
+  emoji.className = "emoji-center";
+  emoji.textContent = "🎉";
+  overlay.appendChild(emoji);
+
+  setTimeout(() => emoji.remove(), 2000);
+
+  // Confettis circulaires
+  const centerX = window.innerWidth / 2;
+  const centerY = window.innerHeight / 2;
+
+  for (let i = 0; i < 30; i++) {
+    const piece = document.createElement("div");
+    piece.className = "piece";
+    piece.style.background = couleurAleatoire();
+
+    // angle sur le cercle
+    const angle = (i / 30) * Math.PI * 2;
+    const radius = 80 + Math.random() * 60;
+
+    const dx = Math.cos(angle) * radius + "px";
+    const dy = Math.sin(angle) * radius + "px";
+
+    piece.style.setProperty("--dx", dx);
+    piece.style.setProperty("--dy", dy);
+
+    piece.style.left = centerX + "px";
+    piece.style.top = centerY + "px";
+
+    overlay.appendChild(piece);
+
+    setTimeout(() => piece.remove(), 2200);
+  };
+
+
+
+  /* appel d ouverture modal*/
+  setTimeout(() => {
   document.dispatchEvent(new Event("Win"));
+  }, 600);
 }
+
+
+
+function couleurAleatoire() {
+  const couleurs = ["#e63946","#f1fa8c","#06d6a0","#118ab2","#ffbe0b"];
+  return couleurs[Math.floor(Math.random() * couleurs.length)];
+}
+
+
+
+
+
+
+
+
 function raise() {
 
   document.dispatchEvent(new Event("raise"));
@@ -39,6 +100,13 @@ export function reveal(cell, index, rows, cols, allCells) {
     cell.style.backgroundColor = "inherit";
     cell.style.backgroundImage = "url(../resources/img/logo_minesweeper.png)";
     cell.style.backgroundSize = "contain";
+
+    const rect = cell.getBoundingClientRect();
+    const x = rect.left + rect.width / 2;
+    const y = rect.top + rect.height / 2;
+
+    lancerAnimBombe(x, y);
+
     gameOver();
   } 
   else if (mineCount > 0) {
@@ -52,6 +120,8 @@ export function reveal(cell, index, rows, cols, allCells) {
 
     raise();
     verifierVictoire(allCells);
+
+    
   } 
   else {
     cell.style.backgroundColor = "white";
@@ -76,6 +146,7 @@ function verifierVictoire(allCells) {
   if (revealedCount === safeCells) {
     console.log("Victoire 🎉");
     gameWin();
+
     return true;
   }
   return false;
